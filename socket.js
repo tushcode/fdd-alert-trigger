88eea68c482aacd7b39696c83180e0cda494c289
Oluwatowo Rosanwo <71358573+CRIMSON-CORP@users.noreply.github.com>
2024-08-21 16:11:55 +0100

    Add files via upload

---
+++ socket.js
@@ -0,0 +1,38 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+/**
+ * @type { WebSocket[]} clients - List of Clients, keyed by an identifier.
+ */
+const clients = [];
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    const data = JSON.parse(message);
+
+    console.log("websocket-action", data);
+
+    // Add client to list if not already present
+    if (!clients.includes(ws)) {
+      clients.push(ws);
+    }
+
+    // Broadcast the message to all connected clients (including the sender)
+    const broadcastMessage = (message) => {
+      clients.forEach((client) => {
+        if (client.ws.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(message));
+        }
+      });
+    };
+
+    broadcastMessage(data);
+  });
+
+  ws.on("close", function close() {
+    const diconnectedClient = clients.find((client) => client === ws);
+  });
+});
+
+console.log("WebSocket server running on ws://localhost:8080");
